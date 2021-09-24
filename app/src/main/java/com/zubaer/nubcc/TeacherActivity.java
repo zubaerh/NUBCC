a8fb402f9b74222d2eb0c320303156e57fcdffbf
ASUS <zubbaerhossain@gmail.com>
2021-09-24 22:27:09 +0600

    teacher dialog added

--- app/src/main/java/com/zubaer/nubcc/TeacherActivity.java
+++ app/src/main/java/com/zubaer/nubcc/TeacherActivity.java
@@ -6,8 +6,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -24,11 +28,13 @@ public class TeacherActivity extends AppCompatActivity {
     private TeacherRecyclerAdapter teacherRecyclerAdapter;
     private LinearLayoutManager linearLayoutManager;
     private ArrayList<Teacher> teacherArrayList;
+    private Button button;
 private DatabaseReference teachers;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher);
+        button = findViewById(R.id.bt_add);
         rec_teacher = findViewById(R.id.rec_teacher);
         teachers= FirebaseDatabase.getInstance().getReference("nubcc").child("teacher");
         teacherArrayList = new ArrayList<>();
@@ -38,6 +44,34 @@ private DatabaseReference teachers;
         rec_teacher.setLayoutManager(linearLayoutManager);
         rec_teacher.setAdapter(teacherRecyclerAdapter);
         loadData();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog = new Dialog(TeacherActivity.this);
+                dialog.setContentView(R.layout.dialog_teacher_add);
+
+               Button button_submit = dialog.findViewById(R.id.bt_submit);
+               EditText deg =  dialog.findViewById(R.id.et_deg);
+               EditText name =  dialog.findViewById(R.id.et_name2);
+               EditText image =  dialog.findViewById(R.id.et_image);
+               EditText phone =  dialog.findViewById(R.id.et_phone);
+               EditText id =  dialog.findViewById(R.id.et_id);
+
+               button_submit.setOnClickListener(new View.OnClickListener() {
+                   @Override
+                   public void onClick(View v) {
+                       Teacher teacher = new Teacher(image.getText().toString(),name.getText().toString(),deg.getText().toString(),Integer.parseInt(phone.getText().toString()));
+                       teachers.child(id.getText().toString()).setValue(teacher);
+                       dialog.cancel();
+                   }
+               });
+
+                dialog.show();
+
+
+            }
+        });
     }
 
     private void loadData(){
